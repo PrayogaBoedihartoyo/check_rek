1022aab5e909604921748f8d0ce0939c805cdc81
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-05-15 10:57:22 +0700

    check_rekeningV1

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const index = express();
 const port = 3000;
 
 index.use(express.json());
-index.use
 index.post('/api/data', async (req, res) => {
     const data = req.body
     const log = new MyModel(data)
@@ -14,6 +13,11 @@ index.post('/api/data', async (req, res) => {
     return res.status(200).json({
         message: "Data added successfully",
     })
+});
+
+index.get('/api/data', async (req, res) => {
+    const allinfo = await MyModel.find({deletedAt: null})
+    res.status(200).json(allinfo);
 });
 
 index.listen(port, () => {
