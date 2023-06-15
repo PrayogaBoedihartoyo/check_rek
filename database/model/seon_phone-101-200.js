c9568d8f872c713819865c186ef50a14208faad1
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-06-15 11:10:48 +0700

    backend-seon-email

---
+++ database/model/seon_phone-101-200.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const moment = require('moment-timezone');
+const timeZone = 'Asia/Bangkok';
+const date = moment.tz(Date.now(), timeZone);
+
+const mySchema = new mongoose.Schema({
+    number: Number,
+    valid: Boolean,
+    score: Number,
+    account_details: {
+        facebook: Boolean,
+        google: Boolean,
+        twitter: Boolean,
+        instagram: Boolean,
+        yahoo: Boolean,
+        microsoft: Boolean,
+        snapchat: Boolean,
+        skype: Boolean,
+        linkedin: Boolean,
+        whatsapp: Boolean,
+        telegram: Boolean,
+        viber: Boolean,
+        kakao: Boolean,
+        ok: Boolean,
+        zalo: Boolean,
+        line: Boolean,
+        flipkart: Boolean,
+        bukalapak: Boolean,
+        jdid: Boolean,
+        tokopedia: Boolean,
+    },
+
+
+    createdAt: {type: Date, default: date},
+    updatedAt: {type: Date, default: date},
+    deletedAt: {type: Date, default: null},
+
+});
+module.exports = mongoose.model('seon_phone-101-200', mySchema)
