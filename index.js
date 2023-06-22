123b83d5018cfb08820a78fd84051c9d708b6088
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-06-22 15:18:19 +0700

    backend-seon

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const check_rekening = require('./database/model/check_rekening');
 const seon_phone = require('./database/model/seon_phone');
 const seon_email = require('./database/model/seon_email');
+const axios = require("axios");
 require('./database/index');
 const index = express();
 const port = 3000;
@@ -54,6 +55,40 @@ index.post('/api/data/seon-email', async (req, res) => {
         res.status(400).json({
             status: "failed",
             message: err.message
+        });
+    }
+});
+
+index.get('/api/check-email-seon-app', async (req, res) => {
+    try {
+        if (!req.query.email) {
+            return res.status(400).json({
+                status: "failed",
+                message: "Email is required"
+            });
+        }
+
+        const headers = {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'X-API-KEY': 'c56baca5-a748-4b1f-bcc6-a456d47510bb',
+        };
+
+        const result = await axios({
+            method: 'get',
+            url: 'https://api.us-east-1-main.seon.io/SeonRestService/email-api/v2.2/' + req.query.email,
+            headers: headers,
+        });
+
+        return res.status(200).json({
+            status: 'success',
+            data: result.data,
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: "failed",
+            message: err.message,
+            detail: err.response.data.message
         });
     }
 });
